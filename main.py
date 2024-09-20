2a3efa1aa9759d5889f02ecff1006f79e45f16ef
pronan <pronan@loyola.edu>
2024-09-20 12:01:23 -0400

    intro comments

--- main.py
+++ main.py
@@ -1,3 +1,11 @@
-# Enter Code here
-# Read the README - under files on left
-# Delete these three lines before submitting
+# Programmers: Donovan and Paige
+# Course:  CS151, Dr. Zelalem Jembre Yalew
+# Due Date: 9/25/2024
+# Lab Assignment: 2
+# Problem Statement: Will determine the expected population in the future (a certain number of years away)
+# for a country based on a current population amount if you are given (1) how often someone is born (in seconds),
+# (2) how often someone dies (in seconds), and (3) how often a new immigrant joins the country (in seconds)
+# Data In: How many second between each birth, how many seconds between each death, how many seconds between each immigration,
+# the current population, how many years in the future
+# Data Out: Whether the population increased or decreased
+# Credits: class resources
