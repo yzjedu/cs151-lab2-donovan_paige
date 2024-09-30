113b5aebb6cc54f2b6aacfed9be87ccb3c970c4a
paigeronan <pronan@loyola.edu>
2024-09-30 16:32:27 -0400

    revision

--- main.py
+++ main.py
@@ -5,10 +5,12 @@
 # Problem Statement: Will determine the expected population in the future (a certain number of years away)
 # for a country based on a current population amount if you are given (1) how often someone is born (in seconds),
 # (2) how often someone dies (in seconds), and (3) how often a new immigrant joins the country (in seconds)
-# Data In: How many second between each birth, how many seconds between each death, how many seconds between each immigration,
-# the current population, how many years in the future
+# Data In: How many second between each birth, how many seconds between each death, how many seconds between each
+# immigration,the current population, how many years in the future
 # Data Out: Whether the population increased or decreased
 # Credits: Class resources
+
+#purpose: the population in a certain amount of years and whether it increases or decreases
 
 #ask user how many seconds between each birth, death, and immigrants
 birth_secs = int(input("How many seconds between each birth?"))
@@ -24,16 +26,19 @@ future_years = int(input("How many years in the future?"))
 secs_per_year = 31536000
 
 #calculation to find the change in population
-change_population = (secs_per_year/ birth_secs + secs_per_year / immigrant_secs - secs_per_year / death_secs) * future_years
+change_population = (secs_per_year/ birth_secs + secs_per_year / immigrant_secs - secs_per_year /
+                     death_secs) * future_years
 
 #calculation to find the future population
 future_population = population + change_population
 
 #outputs the population in the amount of years the user inputs
-print("The population in", future_years, "years will be:", future_population)
+print("The population in", future_years, "years will be:", int(future_population))
 
 #outputs if the population increased or decreased in the time amount of years the user input
 if future_population > population:
     print ("Population increased")
+elif future_population < population:
+    print("Population decreased")
 else:
-    print ("Population decreased")
+    print ("Population stays the same")
